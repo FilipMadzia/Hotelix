5f17c3f28d5bb50acd37a4f7770efbd243e0e317
Filip Madzia <filipmadzia33@gmail.com>
2024-05-25 15:26:51 +0200

    Remove unused imports

--- Hotelix.Web/src/app/app.module.ts
+++ Hotelix.Web/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { HotelsComponent } from './hotels/hotels.component';
 import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
