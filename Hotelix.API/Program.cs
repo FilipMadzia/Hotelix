4a715a4a5f8b5a02670c56828792eca6aad51817
Filip Madzia <filipmadzia33@gmail.com>
2024-05-27 21:57:00 +0200

    Added swagger support for JWT

--- Hotelix.API/Program.cs
+++ Hotelix.API/Program.cs
@@ -3,6 +3,7 @@ using Hotelix.API.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +20,34 @@ builder.Services.AddTransient<UserRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotelix.Api", Version = "v1" });
+
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Type = SecuritySchemeType.Http,
+		BearerFormat = "JWT",
+		In = ParameterLocation.Header,
+		Scheme = "bearer",
+		Description = "Please insert JWT token into field"
+	});
+
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] { }
+		}
+	});
+});
 
 builder.Services.AddAuthentication(options =>
 {
