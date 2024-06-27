a2b4fb2bf2c5abfd3d28379d8245c9497444e828
Filip Madzia <filipmadzia33@gmail.com>
2024-06-27 19:39:50 +0200

    Added city details component with routing and delete button

--- Hotelix.Web/src/app/app-routing.module.ts
+++ Hotelix.Web/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HotelsComponent } from './hotels/hotels.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { CityDetailsComponent } from './admin-panel/city-table/city-details/city-details.component';
 
 const routes: Routes = [
   { path: 'hotel-details/:id', component: HotelDetailsComponent },
   { path: 'hotels', component: HotelsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin-panel', component: AdminPanelComponent },
+  { path: 'admin-panel/city-details/:id', component: CityDetailsComponent },
   { path: '', redirectTo: '/hotels', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
