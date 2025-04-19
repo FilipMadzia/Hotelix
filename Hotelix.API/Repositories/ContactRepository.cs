7ec6ca7e04783661b7a4d43647ac61bf70b836fb
Filip Madzia <filipmadzia33@gmail.com>
2025-04-19 23:55:46 +0200

    Delete Hotelix.API directory

--- Hotelix.API/Repositories/ContactRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿using Hotelix.Api.Data;
-using Hotelix.Api.Data.Entities;
-
-namespace Hotelix.Api.Repositories;
-
-public class ContactRepository(HotelixApiContext context) : BaseRepository<ContactEntity>(context) { }
